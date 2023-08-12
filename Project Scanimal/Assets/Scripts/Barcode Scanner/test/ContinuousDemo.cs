cebee4028f90bed9f6111112bf305d2c0e9b7ca9
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2023-08-12 23:45:50 +0100

    barcode scanner working
    -barcode scanner working correctly but camera texture needs to be fixed

---
+++ Project Scanimal/Assets/Scripts/Barcode Scanner/test/ContinuousDemo.cs
@@ -0,0 +1,107 @@
+﻿using BarcodeScanner;
+using BarcodeScanner.Scanner;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class ContinuousDemo : MonoBehaviour {
+
+	private IScanner BarcodeScanner;
+	public TextMeshProUGUI TextHeader;
+	public RawImage Image;
+	private float RestartTime;
+
+	// Disable Screen Rotation on that screen
+	void Awake()
+	{
+		Screen.autorotateToPortrait = false;
+		Screen.autorotateToPortraitUpsideDown = false;
+	}
+
+	void Start () {
+		// Create a basic scanner
+		BarcodeScanner = new Scanner();
+		BarcodeScanner.Camera.Play();
+
+		// Display the camera texture through a RawImage
+		BarcodeScanner.OnReady += (sender, arg) => {
+			// Set Orientation & Texture
+			Image.transform.localEulerAngles = BarcodeScanner.Camera.GetEulerAngles();
+			Image.transform.localScale = BarcodeScanner.Camera.GetScale();
+			Image.texture = BarcodeScanner.Camera.Texture;
+
+			// Keep Image Aspect Ratio
+			var rect = Image.GetComponent<RectTransform>();
+			var newHeight = rect.sizeDelta.x * BarcodeScanner.Camera.Height / BarcodeScanner.Camera.Width;
+			rect.sizeDelta = new Vector2(rect.sizeDelta.x, newHeight);
+
+			RestartTime = Time.realtimeSinceStartup;
+		};
+	}
+
+	private void StartScanner()
+	{
+		BarcodeScanner.Scan((barCodeType, barCodeValue) => {
+			BarcodeScanner.Stop();
+			if (TextHeader.text.Length > 250)
+			{
+				TextHeader.text = "";
+			}
+			TextHeader.text += "Found: " + barCodeType + " / " + barCodeValue + "\n";
+			RestartTime += Time.realtimeSinceStartup + 1f;
+
+			#if UNITY_ANDROID || UNITY_IOS
+			Handheld.Vibrate();
+			#endif
+		});
+	}
+
+	void Update()
+	{
+		if (BarcodeScanner != null)
+		{
+			BarcodeScanner.Update();
+		}
+
+		// Check if the Scanner need to be started or restarted
+		if (RestartTime != 0 && RestartTime < Time.realtimeSinceStartup)
+		{
+			StartScanner();
+			RestartTime = 0;
+		}
+	}
+
+	#region UI Buttons
+
+	public void ClickBack()
+	{
+		// Try to stop the camera before loading another scene
+		StartCoroutine(StopCamera(() => {
+			SceneManager.LoadScene("Boot");
+		}));
+	}
+
+	/// <summary>
+	/// This coroutine is used because of a bug with unity (http://forum.unity3d.com/threads/closing-scene-with-active-webcamtexture-crashes-on-android-solved.363566/)
+	/// Trying to stop the camera in OnDestroy provoke random crash on Android
+	/// </summary>
+	/// <param name="callback"></param>
+	/// <returns></returns>
+	public IEnumerator StopCamera(Action callback)
+	{
+		// Stop Scanning
+		Image = null;
+		BarcodeScanner.Destroy();
+		BarcodeScanner = null;
+
+		// Wait a bit
+		yield return new WaitForSeconds(0.1f);
+
+		callback.Invoke();
+	}
+
+	#endregion
+}
